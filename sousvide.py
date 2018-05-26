5e9404a99d002d6da173fe535ef62f467da48f79
klogdog <klogdog@users.noreply.github.com>
2018-05-26 16:42:18 -0600

    Update sousvide.py
    added code (to be integrated) and onewire library (part of the standard upy library)

--- sousvide.py
+++ sousvide.py
@@ -1,13 +1,34 @@
 #First generation working thermostat code 
 # using a d1 mini pro and a songle relay triggered on pin 5 (d1)
+#gikfun DS18B20 Output lead: red (VCC), black (GND), yellow (DATA)
+
 
 from wifi import wifi_connect
 from umqtt.simple import MQTTClient
 import machine
 import time
 from machine import Pin
+import onewire
 
+#DS18B20temperatureProbe code
+"""
+#Change device pin to match the GPIO pin that you are using
+#A 4.7kohm resisitor is required however according to https://wp.josh.com/2014/06/23/no-external-pull-up-needed-for-ds18b20-temp-sensor/ you can get away with not using it 
+dataPin = machine.Pin(12)
 
+# create the onewire object
+DS18B20temperatureProbe = onewire.DS18B20(onewire.OneWire(dataPin))
+
+# scan for devices on the bus
+roms = DS18B20temperatureProbe.scan()
+print('found devices:', roms)
+DS18B20temperatureProbe.convert_temp()
+time.sleep_ms(750)
+for rom in roms:
+  print(DS18B20temperatureProbe.read_temp(rom), end=' ')
+  print()
+
+"""
 
 mqttServer = "mqttServer"
 mqttPort = "1883"
